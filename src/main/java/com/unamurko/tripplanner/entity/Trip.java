15f153913fa780b5169dd27cb53cc1ec806f42ae
nunii2 <um33822@student.uni-lj.si>
2026-05-11 14:37:22 +0200

    Added trip entity and trip repo

--- src/main/java/com/unamurko/tripplanner/entity/Trip.java
+++ src/main/java/com/unamurko/tripplanner/entity/Trip.java
@@ -1,4 +1,22 @@
 package com.unamurko.tripplanner.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Entity //this springclass maps to database table
+@Table(name = "trips") //names table trips in PostgreSQl
+@Data
+
 public class Trip {
+
+    @Id // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto increments ID
+    private Long id;
+
+    private String name;
+    private String destination;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
 }
